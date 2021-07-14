de02cf39337f259e79dd253677eac7d0c9fd465c
Adnan <pozegic.adnan@gmail.com>
2021-07-14 12:37:47 +0200

    Add imports

--- src/test/java/PostmanAPITests.java
+++ src/test/java/PostmanAPITests.java
@@ -1,3 +1,9 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 public class PostmanAPITests {
 
 
