dd87f4149733d1fc718d39bfeb9c6433c1f73802
Adnan <pozegic.adnan@gmail.com>
2021-07-14 15:17:22 +0200

    Add createUniqueCollections method and test.

--- src/test/java/PostmanAPITests.java
+++ src/test/java/PostmanAPITests.java
@@ -239,14 +239,252 @@ public class PostmanAPITests {
         }
 
         given()
-                .header( "X-Api-Key", apiKey )
-                .queryParam( "workspace", workspaceId )
-                .body( request.toJSONString() )
-                .when()
-                .post()
-                .then()
-                .statusCode( 400 )
-                .body( "error.name", hasToString( "instanceNotFoundError" ) )
-                .log().all();
+            .header( "X-Api-Key", apiKey )
+            .queryParam( "workspace", workspaceId )
+            .body( request.toJSONString() )
+        .when()
+            .post()
+        .then()
+            .statusCode( 400 )
+            .body( "error.name", equalTo( "instanceNotFoundError" ) )
+            .log().all();
+    }
+
+    @Test
+    public void postmanCreateUniqueCollections() {
+        baseURI = collectionsBaseURI;
+        String workspaceId = "d13fbb01-ecea-49ce-86dd-19ecbf5ec1d5"; // Postman API Tests workspace.
+
+        JSONParser parser = new JSONParser();
+        JSONObject request = new JSONObject();
+
+        // Create: Reqres Testing API collection.
+        try {
+            request = (JSONObject) parser.parse("""
+                    {
+                        "collection": {
+                             "info": {
+                                "name": "Reqres Testing API",
+                                "description": "Reqres is a test resource for REST APIs.",
+                                "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+                             },
+                             "item": [
+                                {
+                                    "name": "List Users",
+                                    "request": {
+                                        "url": "https://reqres.in/api/users?page=1",
+                                        "method": "GET",
+                                        "description": "Lists 6 users per page",
+                                        "header": [],
+                                        "body": {
+                                            "mode": "raw",
+                                            "raw": ""
+                                        }
+                                    },
+                                    "response": []
+                                },
+                                {
+                                    "name": "Single User",
+                                    "request": {
+                                        "url": "https://reqres.in/api/users/1",
+                                        "method": "GET",
+                                        "description": "Gets single user given ID",
+                                        "header": [],
+                                        "body": {
+                                            "mode": "raw",
+                                            "raw": ""
+                                        }
+                                    },
+                                    "response": []
+                                },
+                                {
+                                    "name": "Create User",
+                                    "request": {
+                                        "url": "https://reqres.in/api/users",
+                                        "method": "POST",
+                                        "description": "Creates a new user",
+                                        "header": [
+                                            {
+                                                 "key": "Content-Type",
+                                                 "value": "application/json",
+                                                 "description": ""
+                                            }
+                                        ],
+                                        "body": {
+                                            "mode": "raw",
+                                            "raw": "{\\"name\\": \\"ado387\\", \\"job\\": \\"developer\\"}"
+                                        }
+                                    },
+                                    "response": []
+                                }
+                             ]
+                        }
+                    }
+                    """
+            );
+        } catch ( ParseException e ) {
+            System.out.println( e );
+        }
+
+        given()
+            .header( "X-Api-Key", apiKey )
+            .queryParam( "workspace", workspaceId )
+            .body( request.toJSONString() )
+        .when()
+            .post()
+            .then()
+            .statusCode( 200 )
+            .body( "collection.name", equalTo( "Reqres Testing API") )
+            .log().all();
+
+
+
+        // Create: OpenWeather API collection.
+        try {
+            request = (JSONObject) parser.parse("""
+                    {
+                    	"collection": {
+                    		 "info": {
+                    			"name": "OpenWeather API",
+                    			"description": "Get current data given city name and api key.",
+                    			"schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+                    		 },
+                    		 "item": [
+                    			{
+                    				"name": "Current Weather Sarajevo",
+                    				"request": {
+                    					"url": "api.openweathermap.org/data/2.5/weather?q=Sarajevo&appid=918896278e907ea175546cee3415f5be",
+                    					"method": "GET",
+                    					"description": "Lists 6 users per page",
+                    					"header": [],
+                    					"body": {
+                    						"mode": "raw",
+                    						"raw": ""
+                    					}
+                    				},
+                    				"response": []
+                    			},
+                    			{
+                    				"name": "Current Weather Tuzla",
+                    				"request": {
+                    					"url": "api.openweathermap.org/data/2.5/weather?q=Tuzla&appid=918896278e907ea175546cee3415f5be",
+                    					"method": "GET",
+                    					"description": "Lists 6 users per page",
+                    					"header": [],
+                    					"body": {
+                    						"mode": "raw",
+                    						"raw": ""
+                    					}
+                    				},
+                    				"response": []
+                    			},
+                    			{
+                    				"name": "Current Weather Zenica",
+                    				"request": {
+                    					"url": "api.openweathermap.org/data/2.5/weather?q=Zenica&appid=918896278e907ea175546cee3415f5be",
+                    					"method": "GET",
+                    					"description": "Lists 6 users per page",
+                    					"header": [],
+                    					"body": {
+                    						"mode": "raw",
+                    						"raw": ""
+                    					}
+                    				},
+                    				"response": []
+                    			}
+                    		 ]
+                    	}
+                    }
+                    """
+            );
+        } catch ( ParseException e ) {
+            System.out.println( e );
+        }
+
+        given()
+            .header( "X-Api-Key", apiKey )
+            .queryParam( "workspace", workspaceId )
+            .body( request.toJSONString() )
+        .when()
+            .post()
+            .then()
+            .statusCode( 200 )
+            .body( "collection.name", equalTo( "OpenWeather API") )
+            .log().all();
+
+
+
+        // Create: Postman API collection.
+        try {
+            request = (JSONObject) parser.parse("""
+                    {
+                     	"collection": {
+                     		 "info": {
+                     			"name": "Postman API",
+                     			"description": "Get and create Postman collections.",
+                     			"schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+                     		 },
+                     		 "item": [
+                     			{
+                     				"name": "All Collections",
+                     				"request": {
+                     					"url": "https://api.getpostman.com/collections",
+                     					"method": "GET",
+                     					"description": "Lists all collections.",
+                     					"header": [],
+                     					"body": {
+                     						"mode": "raw",
+                     						"raw": ""
+                     					}
+                     				},
+                     				"response": []
+                     			},
+                     			{
+                     				"name": "Single Collection",
+                     				"request": {
+                     					"url": "https://api.getpostman.com/collections/16604891-8a6c602c-bdaa-4ad7-9f76-b39556f4b925",
+                     					"method": "GET",
+                     					"description": "Fetches single collection given its uid.",
+                     					"header": [],
+                     					"body": {
+                     						"mode": "raw",
+                     						"raw": ""
+                     					}
+                     				},
+                     				"response": []
+                     			},
+                     			{
+                     				"name": "Create Collection",
+                     				"request": {
+                     					"url": "https://api.getpostman.com/collections?workspace=d13fbb01-ecea-49ce-86dd",
+                     					"method": "POST",
+                     					"description": "Create a collection. Body leftout for simplicity.",
+                     					"header": [],
+                     					"body": {
+                     						"mode": "raw",
+                     						"raw": ""
+                     					}
+                     				},
+                     				"response": []
+                     			}
+                     		 ]
+                     	}
+                     }
+                    """
+            );
+        } catch ( ParseException e ) {
+            System.out.println( e );
+        }
+
+        given()
+            .header( "X-Api-Key", apiKey )
+            .queryParam( "workspace", workspaceId )
+            .body( request.toJSONString() )
+        .when()
+            .post()
+            .then()
+            .statusCode( 200 )
+            .body( "collection.name", equalTo( "Postman API") )
+            .log().all();
     }
 }
