ca5f5c9fcd225ec48edf156bdabadd0e20918f91
Adnan <pozegic.adnan@gmail.com>
2021-07-14 11:43:11 +0200

    Initial commit

---
+++ src/test/java/PostmanAPITests.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanAPITests {
+
+
+}
